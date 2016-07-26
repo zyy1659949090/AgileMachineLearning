db52c12565e9449cd667ad447f28d2abfe848914
Allen Grimm <TheGrimmScientist@gmail.com>
2016-07-26 00:10:21 -0700

    Changing output to match what seems to be right, rather than what I'm interpreting the book to say as the right answer.

--- tests/naive_bayes.py
+++ tests/naive_bayes.py
@@ -22,6 +22,6 @@ class TestNaiveBayes(unittest.TestCase):
     def test_party_solutions_with_pre_built_model(self):
         X = ["Near", "No", "Yes"]
         predicted_class = wrapper_for_nb_in_sklearn(PARTY_DATA, X)
-        self.assertEqual(predicted_class, "Study")
+        self.assertEqual(predicted_class, "TV")
 
         #TODO: Probably should have a few other assertions in here.
